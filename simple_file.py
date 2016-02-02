b5e73dba73c5913bd604f752a9792ca0892b6dae
gigi <gigi@dhcp-wlan-public-89-206-109-183.uzh.ch>
2016-02-02 17:53:02 +0100

    change simple file

--- simple_file.py
+++ simple_file.py
@@ -3,6 +3,8 @@
 # Open a file
 fo = open("foo.txt", "wb")
 print "Name of the file: ", fo.name
+
+print "new Line"
+
 print "Closed or not : ", fo.closed
-print "Opening mode : ", fo.mode
 print "Softspace flag : ", fo.softspace
